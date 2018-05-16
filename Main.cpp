21ea686f5d6461844a2526ba708259be3a2656dd
Jose Sandoval <jlsandoval013@gmail.com>
2018-05-15 20:53:08 -0700

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include "Human.h"
+#include "Parent.h"
+#include "Child.h"
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main()
+{
+	Human Dad(string Homer); (36); ('M');
+	Human Mom(string March); (34); ('F');
+	Parent 
+	return 0;
+}
